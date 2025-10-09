24745170eef8381cea072f9b665e546325dd009f
Abhishek Chauhan <159943733+flash-abhi@users.noreply.github.com>
2025-10-09 13:12:32 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cookieParser()); 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://chatify-frontend-rxgg.onrender.com",
     credentials: true
 }));
 
